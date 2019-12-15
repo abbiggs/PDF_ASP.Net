1e6bb1922e82649c8e2553a2f22f98489fb656fd
Austin Biggs <abbiggs@bsu.edu>
2019-12-14 21:04:52 -0500

    Opening local pdf, saving to desktop. Not displaying it in web browser yet.

--- Pdf_In_Browser_1/Default.aspx.cs
+++ Pdf_In_Browser_1/Default.aspx.cs
@@ -9,6 +9,8 @@ using System.IO;
 using System.Drawing.Imaging;
 using System.Drawing;
 using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices;
+
 
 namespace Pdf_In_Browser_1
 {
@@ -16,51 +18,70 @@ namespace Pdf_In_Browser_1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
             //Loads the Pdf document
-            //Current error occurs here
-            PDFiumSharp.PdfDocument document = new PdfDocument("~/Images/blank.pdf");
-            //Collection of the documents pages
-            PDFiumSharp.PdfPageCollection pageCollection = document.Pages;
-            //Selects the first page
-            PDFiumSharp.PdfPage page = pageCollection[0];
+            PDFiumSharp.PdfDocument document = null;
+            PDFiumSharp.PdfPageCollection pageCollection = null;
+            PDFiumSharp.PdfPage page = null;
+            PDFiumSharp.PDFiumBitmap bitmap = null;
+            try
+            {
+                document = new PdfDocument("C:/Users/ab716/Desktop/PDF_InBrowserRendering/PDF's/annotation links.pdf");
+                System.Diagnostics.Debug.WriteLine("File Version: " + document.FileVersion.ToString());
+                //document.Close();
 
-            //Creates a bitmap, and renders the first page to the bitmap
-            PDFiumSharp.PDFiumBitmap bitmap = new PDFiumBitmap(500, 500, true);
-            page.Render(bitmap);
+                pageCollection = document.Pages;
+                System.Diagnostics.Debug.WriteLine("Number of pages: " + pageCollection.Count);
 
+                bitmap = new PDFiumBitmap(500, 500, true);
 
+                page = pageCollection[0];
+
+                page.Render(bitmap);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
             //Saves the bitmap to a memory stream
             //Draws an image from the stream, and attempts to save it to the projects Images folder
-            //After saving, attempts to display the image in the web pages image control
-            using (var Stream = new MemoryStream())
+            //After saving, attempts to display the image in the web pages' image control
+            try
             {
-                bitmap.Save(Stream, 500, 500);
-
-                using (System.Drawing.Image image = System.Drawing.Image.FromStream(Stream))
+                using (var Stream = new MemoryStream())
                 {
-                    // Upon success image contains the bitmap
-                    // And can be saved to a file:
-                    try
-                    {
-                        //If it makes it past the error when loading the document, another 
-                        //Error occurs here
-                        image.Save("~/Images/testImage", System.Drawing.Imaging.ImageFormat.Bmp);
-                        Image1.ImageUrl = "~/Images/testImage.Bmp";
-                    }
-                    catch (System.Exception ex)
-                    {
-                        ex.ToString();
-                        Console.WriteLine(ex);
-                    }
+                    bitmap.Save(Stream, 500, 500);
 
+                    using (System.Drawing.Image image = System.Drawing.Image.FromStream(Stream))
+                    {
+                        // Upon success image contains the bitmap
+                        // And can be saved to a file:
+                        try
+                        {
+
+                            image.Save("C:/Users/ab716/Desktop/test3.png");
+
+                            //This isn't the image that's being saved, this is just a test image that's in the project directory, still have work to do here
+                            Image1.ImageUrl = "~/Images/test.jpg";
+                        }
+                        catch (System.Exception ex)
+                        {
+                            ex.ToString();
+                            Console.WriteLine(ex);
+                        }
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+
         }
     }
 }
