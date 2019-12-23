c910fcd218cee0e0bc10e8fd7deb30ac58508e25
Austin Biggs <abbiggs@bsu.edu>
2019-12-23 16:43:41 -0500

    Added more CSS to improve styling/layout.

--- Pdf_In_Browser_1/Default.aspx.designer.cs
+++ Pdf_In_Browser_1/Default.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace Pdf_In_Browser_1
     {
 
         /// <summary>
-        /// customViewer control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl customViewer;
-
-        /// <summary>
         /// btnLoadPdf control.
         /// </summary>
         /// <remarks>
