709538f8fa23a00a56ca8b89fa5998df644a8dbd
Evan Witous <evanwitous@gmail.com>
2020-01-07 13:52:39 -0500

    Pushing again, because it didn't add this folder last time

---
+++ Pdf_In_Browser_1/Scripts/Frontend/FrontendControls.js
@@ -0,0 +1,14 @@
+﻿function jumpToPage(event) {
+    //Executes on enter key press event
+    if (event.which == 13 || event.keyCode == 13) {
+        try {
+            var pageNum = document.getElementById("manualPageInput").value;
+            var actualPageNum = pageNum - 1;
+            document.getElementById("MainContent_img" + actualPageNum).scrollIntoView();
+            return false;
+        } catch (error) {
+            alert("Page Number Out Of Bounds.");
+            return false;
+        }
+    }
+}
