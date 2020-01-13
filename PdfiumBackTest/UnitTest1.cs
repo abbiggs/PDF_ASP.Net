1d4247b3eb115bbd2824823809363976fbe4cf86
Evan Witous <evanwitous@gmail.com>
2020-01-13 16:23:44 -0500

    Started adding more abstraction, waiting till after presention

--- PdfiumBackTest/UnitTest1.cs
+++ PdfiumBackTest/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace PdfiumBackTest {
     public class UnitTest1 {
 
         [TestMethod]
-        public void TestMethod1() {
+        public void TestPdfToImageByPage() {
 
         }
     }
