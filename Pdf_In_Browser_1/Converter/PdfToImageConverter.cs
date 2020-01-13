31803cd5cf60a8020fa52ab4f4a57345244d15d4
Evan Witous <evanwitous@gmail.com>
2020-01-13 16:23:44 -0500

    Started adding more abstraction, waiting till after presention

---
+++ Pdf_In_Browser_1/Converter/PdfToImageConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pdf_In_Browser_1 {
+    public class PdfToImageConverter {
+
+        //Put all code that deals with converting the PDF to img here
+        //Put all the code that deals with adding the img to html in Default.aspx.cs
+    }
+}
