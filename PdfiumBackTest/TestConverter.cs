0ec1d4ab9b9fff19548f7be9fe89f7bd1f236568
Evan Witous <evanwitous@gmail.com>
2020-01-16 16:44:59 -0500

    Work on unit tests

--- PdfiumBackTest/TestConverter.cs
+++ PdfiumBackTest/TestConverter.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pdf_In_Browser_1;
+using PDFiumSharp;
 
 namespace PdfiumBackTest {
 
@@ -7,23 +11,38 @@ namespace PdfiumBackTest {
     public class TestConverter {
 
         [TestMethod]
-        public void TestPdfToImageByPage() {
+        public void TestFindingPdf() {
 
             //Find pdf doc
             //Set to pdfdocument variable
             //create instance of PdfToImageConverter
             //Convert pdfdocument to image
             //assert that returned object is an image datatype
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            PdfDocument document;
+
+            path = path + @"\TestDocuments\testdoc.pdf";
+            document = new PdfDocument(path);
+
+            Assert.IsNotNull(document);
         }
 
         [TestMethod]
-        public void TestPdfToImageArray() {
+        public void TestConvertingToImage() {
 
-            //Find pdf doc
-            //Set to pdfdocument variable
-            //create instance of PdfToImageConverter
-            //Convert pdfdocument to imageArray
-            //assert that array is correct length
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            PdfToImageConverter converter = new PdfToImageConverter();
+            PdfDocument document;
+            Image result;
+
+            path = path + @"\TestDocuments\testdoc.pdf";
+            document = new PdfDocument(path);
+
+            result = converter.pdfToImageByPage(0, document);
+
+            Assert.IsNotNull(result);
         }
     }
 }
+
+
