ca17dda87a401f9fd1d334ab9cf5b2027c21c1de
Austin Biggs <abbiggs@bsu.edu>
2020-01-30 11:04:15 -0500

    Added model class and api controller class

---
+++ Pdf_In_Browser_1/PdfPageImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.UI.HtmlControls;
+using Pdf_In_Browser_1.TextExtraction;
+using PDFiumSharp;
+
+namespace Pdf_In_Browser_1
+{
+    public class PdfPageImage
+    {
+        public String name { get; set; }
+
+    }
+}
