7bb550addced4bac98983a1ebb07a8e1b5eebb2f
evanwitous@gmail.com <evanwitous@gmail.com>
2020-03-09 18:19:57 -0400

    Worked on redactions

--- Pdf_In_Browser_1/Scripts/Frontend/AnnotationControls.js
+++ Pdf_In_Browser_1/Scripts/Frontend/AnnotationControls.js
@@ -1,1 +1,28 @@
-﻿
+﻿var redacting = false;
+var container = document.getElementsByClassName("leftImageContainer");
+
+activateRedaction() {
+
+    if (redacting != true) {
+
+        redacting = true;
+        container.addEventListener("click", setPosition, false);
+    }
+}
+
+setPosition(e) {
+    var xPos = e.clientX;
+    var yPos = e.clientY;
+
+    addElement(xPos, yPos);
+}
+
+addElement(x, y) {
+    var redaction = document.createElement("span");
+
+    redaction.setAttribute("class", "redaction");
+    redaction.style.top = yPos;
+    redaction.style.left = xPos;
+
+    container.appendChild(redaction);
+}
