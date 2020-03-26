cb06db64ca781793381f0341cd0503c56f7de8af
Austin Biggs <abbiggs@bsu.edu>
2020-03-25 21:01:34 -0400

    Fixed zooming issue.

--- Pdf_In_Browser_1/Scripts/Frontend/ZoomingControls.js
+++ Pdf_In_Browser_1/Scripts/Frontend/ZoomingControls.js
@@ -1,35 +1,62 @@
 ﻿var zoomFactor = 1.0;
+var toolZoom = 1.0;
+let customViewerL = $("#MainContent_customViewerL");
+let toolbar = $("#MainContent_actionButtonsContainer");
+
 
 function zoomIn() {
     let body = $('body');
     
     zoomFactor += .1;
-    containerMargin = ((zoomFactor - 1.0) * 100);
-
-    if (zoomFactor <= 3.0) {
-        //customViewerL.css('transform', 'scale(' + zoomFactor + ')');
-        //customContainer.css('margin-top', '' + containerMargin + '%');
-
-        body.css('transform', 'scale(' + zoomFactor + ')');
-
-        //let toolbarZoom = (zoomFactor * 0.5);
-        //toolbar.css('transform', 'scale(' + toolbarZoom + ')');
-    } else {
-        zoomFactor -= 1;
-    }
     
+    if (zoomFactor <= 1.5) {
+        
+        body.css('transform', 'scale(' + zoomFactor + ')');
+        
+    } else {
+        zoomFactor -= 0.5;
+        body.css('transform', 'scale(' + zoomFactor + ')');
+        toolbar.css('transform', 'scale(' + zoomFactor + ')');
+        zoomFactor = 1.0;
+        toolZoom = 1.0;
+        return false;
+    }
+
+    if (zoomFactor <= 1.3) {
+        toolZoom -= .08;
+        toolbar.css('transform', 'scale(' + toolZoom + ')');
+    } else {
+        toolZoom -= .06;
+        toolbar.css('transform', 'scale(' + toolZoom + ')');
+    }
+
     return false;
 }
 
 function zoomOut() {
-    //zoom out max is 0.4
-    //let element = $('#MainContent_customViewerL');
-    let element = $('body')
-    zoomFactor -= .1;
-    
-    if (zoomFactor >= 0.4) {
-        element.css('transform', 'scale(' + zoomFactor + ')');
-    } else {
-        zoomFactor += 1;
+    let body = $('body');
+
+    if (zoomFactor > 1.0) {
+        zoomFactor -= .1;
+
+        if (zoomFactor >= 1.0) {
+
+            body.css('transform', 'scale(' + zoomFactor + ')');
+
+        } else {
+        
+            return false;
+        }
+
+        if (zoomFactor > 0.9) {
+            toolZoom += .08;
+            toolbar.css('transform', 'scale(' + toolZoom + ')');
+        } else if (zoomFactor >= 1.24) {
+            toolZoom += .06;
+            toolbar.css('transform', 'scale(' + toolZoom + ')');
+        }
     }
+    
+
+    return false;
 }
